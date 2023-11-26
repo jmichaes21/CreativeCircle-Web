9b99f6a499ba656b232460cf88ad5811aa9e0015
Ajoydude <s22110143@student.unklab.ac.id>
2023-11-26 20:28:17 +0800

    mod: Billing and CardDeals text

--- src/components/Billing/index.jsx
+++ src/components/Billing/index.jsx
@@ -15,11 +15,11 @@ const Billing = () => {
         <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient'/>
       </div>
       <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>Easily control your <br className='sm:block hidden'/> billing & invoicing.</h2>
+        <h2 className={styles.heading2}>Look Up Your <br className='sm:block hidden'/> "Criteria Met" Designer.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Elit enim sed massa etiam.
-          Mauris eu adipiscing ultrices ametodio aenean neque.
-          Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+          Easily find your desired designer
+          with skills and service that lines up with your needs.
+          
         </p>
         <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
           <img
--- src/components/CardDeals/index.jsx
+++ src/components/CardDeals/index.jsx
@@ -7,12 +7,9 @@ const CardDeal = () => {
   return (
     <section className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/>in few easy steps.</h2>
+        <h2 className={styles.heading2}>Simple and Easy <br className='sm:block hidden'/>Communication Feature.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Arcu tortor,
-          purus in mattis at sed integer faucibus.
-          Aliquet quis aliquet eget mauris tortor.ç 
-          Aliquet ultrices ac, ametau.
+          Discuss the service you require from the designer, with build in communication tool.
         </p>
         <Button styles='mt-10'/>
       </div>
