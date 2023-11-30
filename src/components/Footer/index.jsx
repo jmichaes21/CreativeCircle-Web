5fbf718c25e850cf01ddf05a367fd78df1f0db9b
poetriiij <kalantowwpoetri@gmail.com>
2023-11-30 21:13:43 +0800

    modify: Footer text

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
             className='w-[266px] h-[172px] object-contain'
           />
           <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
-            A new way to make the payments easy, reliable and secure.         
+            A new way to connect customer and designer.         
           </p>
         </div>
         <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
