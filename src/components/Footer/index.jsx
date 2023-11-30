4fbe508c6803261a165825cbd02290a6a8fee047
jmichaes21 <juansalainti@gmail.com>
2023-11-30 21:05:57 +0800

    fix: Download App Link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
       const data = snapshot.val();
       setsocialMedia(data);
     })
-  })
+  }, [])
 
 
   return (
--- src/components/GetStarted/index.jsx
+++ src/components/GetStarted/index.jsx
@@ -5,8 +5,10 @@ import { arrowUp } from '../../assets/images'
 const GetStarted = () => {
   return (
     <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+      
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
-        <div className={`${styles.flexStart} flex-row`}>
+      <a href="https://play.google.com/store/games?hl=id&gl=US&pli=1">
+      <div className={`${styles.flexStart} flex-row`}>
           <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
             <span className='text-gradient'>Download</span>
           </p>
@@ -16,6 +18,8 @@ const GetStarted = () => {
             className='w-[23px] h-[23px] object-contain'
           />
         </div>
+      </a>
+        
         <p className='font-poppins font-medium text-[18px] leading-[23px]'>
           <span className='text-gradient'>App</span>
         </p>
